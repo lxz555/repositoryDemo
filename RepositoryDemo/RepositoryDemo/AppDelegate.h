a825a1ca7871480bf962b6910f91cc083365f02d
李雪智 <xuezhi.li@husor.com>
2016-04-28 11:59:49 +0800

    first_commit

---
+++ RepositoryDemo/RepositoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RepositoryDemo
+//
+//  Created by 李雪智 on 16/4/28.
+//  Copyright © 2016年 李雪智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RepositoryDemo/RepositoryDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RepositoryDemo
+//
+//  Created by 李雪智 on 16/4/28.
+//  Copyright © 2016年 李雪智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
